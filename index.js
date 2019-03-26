86c45efc3b5b6dce1ba2ecfa965e0f1438356dc5
E-man <emmanuel.donohue@gmail.com>
2019-03-26 14:35:51 -0700

    added filter for ufo data

--- index.js
+++ index.js
@@ -1,76 +1,68 @@
 //First, working function
 var tableData = data;
 
+// Get a reference to the table body. Find the reference in the .html file 
 var tbody = d3.select("tbody");
 
+
 function buildTable(data) {
-  // Get a reference to the table body. Find the reference in the .html file 
-  
 
-  // // Step 1: Loop Through `data` and console.log each ufo report object
-  tableData.forEach(function dataToHtml(ufoReport) {   //this is the working anon function
-  //function dataToHtml(ufoReport) {
+  // First, clear out any existing data
+  tbody.html("");
+
+  // Loop Through `data` and console.log each ufo report object
+  tableData.forEach(function dataToHtml(ufoReport) {   
+
     console.log(ufoReport);     
-  //!!} //delete curly?
 
-  // // Step 2:  Use d3 to append one table row `tr` for each ufo report object
 
+  // Use d3 to append one table row `tr` for each ufo report object
     var new_row = tbody.append("tr");
-       //bc we added new row, we move over the entries of the new object and ...
-  //have to create place to put data before appending data
-  // // Step 3:  Use `Object.entries` to console.log each ufo report value
 
+  // Use `Object.entries` to console.log each ufoReport value
      Object.entries(ufoReport).forEach(function([key, value]) {
        console.log(key, value)
 
-  // // Step 4: Use d3 to append 1 cell per ufo report value (datetime, city, state, country, shape, durationMinutes, comments)
-
-  //     // Append a cell to the row for each value
-  //     // in the ufo report object
+  // Use d3 to append 1 cell per ufoReport value
        var cell = tbody.append("td");
 
-  // // Step 5: Use d3 to update each cell's text with
-  // // ufo report values (weekday, date, high, low)
-
+  // Use d3 to update each cell's text with ufoReport values
        cell.text(value);
      });
   });
-    // ****
-    // **** 
 };
 
+
+
+var flt = d3.select("#filter-btn");
+
+// Grab the datetime value from the filter
+var dateVar = d3.select("#datetime");    
 
 
 function handleClick() {
 
-  // Prevent the form from refreshing the page
+  //prevent page from refreshing
   d3.event.preventDefault();
 
   // Grab the datetime value from the filter
-  // ****
-  // ****
-  dateVar = document.getElementById('datetime').value;
-  console.log(dateVar);
+  //var userInput = dateVar.value();
+  //var userInput = dateVar.property.value;
+  //var userInput = document.dateVar.value();
+  //var userInput = dateVar.property("value");
+  //var userInput = document.property("value");
+  //var userInput = dateVar.value.trim();
+  var userInput = document.getElementById('datetime').value;
 
-  // Check to see if a date was entered and filter the
-  // data using that date.
-  // **** if condition
-  if (dateVar != "") { 
-    // Apply `filter` to the table data to only keep the
-    // rows where the `datetime` value matches the filter value
-    // ****
-    console.log("Someone entered something")
-    //var filteredData = tableData.filter(checkDate);
-    filteredData = tableData.filter(tableData=>tableData.datetime==dateVar);
-};
-  // Rebuild the table using the filtered data
-  // @NOTE: If no date was entered, then filteredData will
-  // just be the original tableData.
+  var filteredData = tableData.filter(tableData => tableData.datetime === userInput);
+  
   buildTable(filteredData);
 };
 
 // Attach an event to listen for the form button
 d3.selectAll("#filter-btn").on("click", handleClick);
-
 // Build the table when the page loads
-buildTable(data);
+buildTable(tableData);
+
+
+
